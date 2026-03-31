65a4d9f1a8f021d09414d1f7bcfb4c76495bae80
4aek-dotru <andrey12112005@yandex.ru>
2026-03-31 11:32:33 +0300

    end

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ const button = document.querySelector('.reset-button-inactive');
 const image = document.getElementById('image-code');
 
 button.onclick = getUrlPhoto;
-    
-
+let history = [];
+let currentImage = -1;
+document.addEventListener('keydown', e => {
+    scrollImages(e);
+})
 // Получение URL через API, с последующей генерацией картинки
 async function getUrlPhoto() {
     const accessToken = "live_JrDm6O81DcwdMqpYaRKNJyN3Cnu8ok8hwKYnTYGG87GA16Nez6PMMgEio5fbJ81a";
@@ -18,13 +21,31 @@ async function getUrlPhoto() {
         )
         
         const data = await responce.json();
+        history.push(data[0].url);
+        currentImage++;
         
         button.classList.remove('reset-button-active');
         button.classList.add('reset-button-inactive');
-        image.innerHTML = `<img  id='image' src='${data[0].url}'></div>`
+        image.innerHTML = `<img  id='image' src='${data[0].url}'></div>`;
 
     }   catch (error) {
         console.error('Ошибка загрузки:', error);
         return '1';
     }
+}
+function scrollImages(e) {
+   if(e.key == 'ArrowRight') {
+    if (currentImage == history.length -1){
+        getUrlPhoto();
+    }else {
+        currentImage++;
+        image.innerHTML = `<img  id='image' src='${history[currentImage]}'></div>`;
+    }
+    }
+    if(e.key == 'ArrowLeft') {
+        if(currentImage == 0) return
+        currentImage--;
+        image.innerHTML = `<img  id='image' src='${history[currentImage]}'></div>`;
+    }
+    console.log(e)
 }
